976b921b0734761410d6b72b6de4ba621d470124
Sirichai.Janpan <boy.sirichai@bitkub.com>
2022-08-17 18:21:40 +0700

    fixed router

--- src/Generate/template/Route.php
+++ src/Generate/template/Route.php
@@ -3,14 +3,10 @@
 $router->group(
     array(
         'prefix' => 'v1/{replace_sm}s',
-        'middleware' => array('jwt.auth'),
-        'namespace' => 'API\V1\{replace}',
+        'middleware' => array('login'),
+        'namespace' => '{replace}',
     ),
     function () use ($router) {
-        $router->get('/', array('uses' => '{replace}Controller@index'));
-        $router->post('/', array('uses' => '{replace}Controller@store'));
-        $router->get('{id}', array('uses' => '{replace}Controller@show'));
-        $router->put('{id}', array('uses' => '{replace}Controller@update'));
-        $router->delete('{id}', array('uses' => '{replace}Controller@destroy'));
+        $router->get('/', array('uses' => '{replace}Controller@{replace}Functional'));
     }
 );
